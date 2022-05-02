ecca08b1011f05a7d0201c9b6d8a90c5a115b991
RottenSailer <satwikshresth@gmail.com>
2022-05-01 23:39:05 -0400

    done

--- createDatabase.py
+++ createDatabase.py
@@ -1,7 +1,8 @@
 import os, exifread, shutil ,time, rawpy, tempfile, cv2
+import numpy as np
+from posixpath import split
 from PIL import Image
-from pathlib import Path
-
+from pathlib import Path 
 
 class application:
 
@@ -10,32 +11,78 @@ class application:
                                     '.mef', '.nef', '.orf', '.rw2', '.pef', '.x3f', '.srw', '.srf', '.sr2', '.arw',
                                     '.mdc', '.mrw', '.jpg', '.png')
         self.face_cascade = cv2.CascadeClassifier("E:\project\FOrg\haarcascade\haarcascade_frontalface_alt.xml")
-        self.tag = {}
+        self.face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+        self.valueTags = [
+            "EXIF FocalLength",
+            "EXIF ISOSpeedRatings",
+            "EXIF FNumber",
+            "EXIF DateTimeDigitized",
+        ]
+        self.faces= []
+        self.labels=[]
+        self.subject = ["","riya"]
 
-    def checkTagExist(self,exifTagName):
-        try:
-            return(self.tags[exifTagName])
-        except:
-            return  
+    
+    def predict(self,test_img):
+        img = test_img.copy()
+        face, rect = self.detect_face(img)
+        label, confidence = self.face_recognizer.predict(face)
+        label_text = self.subjects[label]
+        self.draw_rectangle(img, rect)
+        self.draw_text(img, label_text, rect[0], rect[1]-5)
+        return img
+    
+    def draw_rectangle(self,img, rect):
+        (x, y, w, h) = rect
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    def draw_text(self,img, text, x, y):
+        cv2.putText(img, text, (x, y), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 255, 0), 2)
+
+    def detect_face(self,img):
+        image_array =np.array(img,"uint8")
+        faces = self.face_cascade.detectMultiScale(image_array,scaleFactor=1.5 )
+        if (len(faces) == 0):
+            return None, None
+        (x, y, w, h) = faces[0]
+        return image_array[y:y+w, x:x+h], faces[0]
+
+    def train_algo(self,database):
+        labels = []
+        for image_name in database.values():
+            print(image_name["root"])
+            if image_name["root"].endswith(("jpg","jpeg")):
+                image = Image.open(image_name["root"]).convert("L")
+                face, label = self.detect_face(image)
+            if face is not None:
+                self.faces.append(face)
+                print(label)
+                labels.append(label)
+        print(np.array(labels))
+        l = np.array(labels)
+        self.face_recognizer.train(self.faces,l)
+        
 
     def createDatabase(self,dir):
         Database = {}
         for root, dirs, files in os.walk(dir):
             for file in files:
                 if file.endswith(self.extensions):
-                    location = f"{root}\\{file}"
+                    location = os.path.join(root,file)
                     f = open(location, 'rb')
                     self.tags = exifread.process_file(f)
+
                     Database[file] = {
                         "root" : location,
                         "ext"  : Path(file).suffix,
-                        "Model" :  if tags["Image Model"] ("%s" % tags["Image Model"]),
-                        "FocalLength": ("%s" % tags["EXIF FocalLength"]),
-                        "ISO": ("%s" % tags["EXIF ISOSpeedRatings"]),
-                        "Fstop": ("%s" % tags["EXIF FNumber"]),
-                        "Date & Time": ("%s" % tags["EXIF DateTimeDigitized"]),
                         "Potrait": self.checkPotrait(location)
                         }
+                    for i in self.valueTags:
+                        taa =i.split(" ")
+                        try:
+                            Database[file][taa[-1]] = ("%s" % self.tags[i])
+                        except:
+                            continue
                     f.close()
         return Database
     
@@ -48,9 +95,9 @@ class application:
         faces = self.face_cascade.detectMultiScale(gray,1.1,4)
         numface = len(faces)
         if numface == 0:
-            return "Potrait"
-        else:
             return "Landscape"
+        else:
+            return "Potrait"
 
     def view_preview(self,address):
         with rawpy.imread(address) as raw:
@@ -68,23 +115,32 @@ class application:
     
     def process_photo(self,database,sortValue,targetPath):
         processedPhotos = 0
+        notprocessedPhotos = 0
         for file,tags in database.items():
-            destinationFolder = tags[sortValue]
-            if not os.path.isdir(os.path.abspath(os.path.join(targetPath, destinationFolder))):
-                os.makedirs(os.path.abspath(os.path.join(targetPath, destinationFolder)))
-            newName = os.path.abspath(os.path.join(targetPath, destinationFolder,file))
-            shutil.move(tags["root"],newName)
-            processedPhotos += 1
-            print("\r%d photos processed" % (processedPhotos), end='')
+            try:
+                destinationFolder = tags[sortValue]
+                if not os.path.isdir(os.path.abspath(os.path.join(targetPath, destinationFolder))):
+                    os.makedirs(os.path.abspath(os.path.join(targetPath, destinationFolder)))
+                newName = os.path.abspath(os.path.join(targetPath, destinationFolder,file))
+                shutil.move(tags["root"],newName)
+                processedPhotos += 1
+            except:
+                notprocessedPhotos +=1
+            else:
+                print("\r%d photos processed" % (processedPhotos), end='')
+                print("\r%d photos processed" % (notprocessedPhotos), end='')
         
         
 def main(argv=None):
     tic = time.perf_counter()
-    dir = r"E:\testfile"
+    dir = r"E:\project\FOrg\images"
     app = application()
     database = app.createDatabase(dir)
     print("There are total %d files" % len(database))
     app.process_photo(database, "Portrait",r"E:\project\lol")
+    app.train_algo(database)
+    test = cv2.imread(r"E:\project\FOrg\D2009094.jpg")
+    app.predict(test)
     toc = time.perf_counter()
     print(f"Time used: {toc - tic:0.4f} seconds")
 
